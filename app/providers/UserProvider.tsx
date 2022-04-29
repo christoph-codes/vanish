86af5f3fac8ea42ed395df6ca81efe99d5bf3b3e
Christopher Jones <tkcwebdev@gmail.com>
2022-04-28 21:12:18 -0700

    initiate: Add user provider data with dummy data

--- app/providers/UserProvider.tsx
+++ app/providers/UserProvider.tsx
@@ -1,33 +1,56 @@
 import { useContext, createContext, useState } from "react";
-import { signInWithEmailAndPassword } from "firebase/auth";
+import { signInWithEmailAndPassword, createUserWithEmailAndPassword } from "firebase/auth";
 import { auth } from "../config/firebase";
+import users from "../util/users";
 
 const UserContext = createContext<any>([]);
 
 export default function UserProvider({ children }: any) {
-    const [user, setUser] = useState({});
+    const [activeUser, setActiveUser] = useState<IUser | null>();
     const [error, setError] = useState<{
         code: string,
         message: string,
     } | null>();
+
     const signIn = (email: string, password: string) => {
         if (email !== '' && password !== '') {
-            signInWithEmailAndPassword(auth, email, password)
-                .then((data) => {
-                    console.log('loggedin:', data);
-                    setError(null);
+            // TODO: Delete this check
+            if (process.env.NODE_ENV === 'development') {
+                const existingUser: any = users.filter(usr => {
+                    if (usr?.email === email) {
+                        return usr;
+                    }
+                    return null;
                 })
-                .catch(err => setError({
-                    code: '001',
-                    message: `Login err: ${err}`
-                }));
+                console.log(existingUser);
+                if (existingUser?.password === password) {
+                    setActiveUser(existingUser);
+                } else {
+                    setError({ code: '110', message: 'These user credentials do match anything in our records.' })
+                }
+            } else {
+                signInWithEmailAndPassword(auth, email, password)
+                    .then((data) => {
+                        console.log('loggedin:', data);
+                        setError(null);
+                    })
+                    .catch(err => setError({
+                        code: '001',
+                        message: `Login err: ${err}`
+                    }));
+            }
         }
     }
     const logout = () => {
-        setUser({});
+        setActiveUser(null);
+    }
+    const createAccount = (email: string, password: string) => {
+        createUserWithEmailAndPassword(auth, email, password).then(result => {
+            console.log('completed:', result);
+        })
     }
     return (
-        <UserContext.Provider value={{ user, error, setUser, setError, logout, signIn }}>
+        <UserContext.Provider value={{ activeUser, error, setActiveUser, setError, logout, signIn, createAccount }}>
             {children}
         </UserContext.Provider>
     )
