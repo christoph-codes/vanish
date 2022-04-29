a536a7f95c1c115727ecb2ae8b9dbaf5682004fe
Christopher Jones <tkcwebdev@gmail.com>
2022-04-28 21:10:38 -0700

    initiate: Add native base to project

--- app/App.tsx
+++ app/App.tsx
@@ -1,29 +1,53 @@
 import React from 'react'
+import { NativeBaseProvider, extendTheme } from 'native-base';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import routes from './routes';
 import UserProvider from './providers/UserProvider';
 
-const Stack = createNativeStackNavigator();
+const Stack: any = createNativeStackNavigator();
 
 export default function App() {
+    const theme = extendTheme({
+        colors: {
+            brand: {
+                primary: '#222',
+                secondary: '#777',
+                tertiary: '#999',
+                light: '#eee',
+            }
+        },
+        components: {
+            Heading: {
+                // Can pass also function, giving you access theming tools
+                baseStyle: ({ colorMode }: any) => {
+                    return {
+                        color: colorMode === "dark" ? "red.300" : "blue.300",
+                        fontWeight: "normal",
+                    };
+                },
+            },
+        },
+    });
     return (
-        <UserProvider>
-            <NavigationContainer>
-                <Stack.Navigator initialRouteName="Home">
-                    {routes.map(({ name, component, options }, index) => {
-                        return (
-                            <Stack.Screen
-                                key={index}
-                                name={name}
-                                component={component}
-                                options={options}
-                            />
-                        )
-                    })}
-                </Stack.Navigator>
-            </NavigationContainer>
-        </UserProvider>
+        <NativeBaseProvider theme={theme}>
+            <UserProvider>
+                <NavigationContainer>
+                    <Stack.Navigator initialRouteName="Home">
+                        {routes.map(({ name, component, options }, index) => {
+                            return (
+                                <Stack.Screen
+                                    key={index}
+                                    name={name}
+                                    component={component}
+                                    options={options}
+                                />
+                            )
+                        })}
+                    </Stack.Navigator>
+                </NavigationContainer>
+            </UserProvider>
+        </NativeBaseProvider>
     )
 }
 
